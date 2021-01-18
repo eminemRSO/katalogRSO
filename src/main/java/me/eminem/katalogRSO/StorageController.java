6dd146511003a4da4a904a4b6b8eeaebb57e1bc9
M <m@bass.si>
2021-01-18 07:18:43 +0100

    master ingress + ingress deployment + nekaj poskusov circuit breakerjev + sprememba sql

--- src/main/java/me/eminem/katalogRSO/StorageController.java
+++ src/main/java/me/eminem/katalogRSO/StorageController.java
@@ -1,9 +1,12 @@
 package me.eminem.katalogRSO;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -25,4 +28,30 @@ public class StorageController {
     public Stream<TaskDTO> getAllTasks(){
         return taskService.getAllTasks().stream().map(task -> modelMapper.map(task,TaskDTO.class));
     }
+
+
+    //hmm
+    @Retryable(maxAttempts = 5)
+    @GetMapping(path="/tasksRetry")
+    public Stream<TaskDTO> getAllTasksRetry() throws Exception {
+        throw new Exception("Exception message");
+        //return taskService.getAllTasks().stream().map(task -> modelMapper.map(task,TaskDTO.class));
+    }
+    @Recover
+    public String weRecovered(){
+        return "we fall backed to this after 5 tries";
+    }
+
+//    @HystrixCommand(fallbackMethod = "reliable")
+//    @GetMapping(path="/fallbackTest")
+//    public String readingList() {
+//        URI uri = URI.create("http://localhost:8090/recommended");
+//
+//        return this.restTemplate.getForObject(uri, String.class);
+//    }
+//
+//    public String fallback(){
+//        return taskService.getAllTasks().stream().map(task -> modelMapper.map(task,TaskDTO.class));
+//    }
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tasks VALUES (1,'Naredi čaj',0.99);
-INSERT INTO tasks VALUES (2,'Kupi čaj',15.99);
-INSERT INTO tasks VALUES (3,'Scedi čaj',4.99);
-INSERT INTO tasks VALUES (4,'Popij čaj',2.99);
+INSERT INTO tasks VALUES (1,'Rišem hitro',0.99);
+INSERT INTO tasks VALUES (2,'Rišem počasi',15.99);
+INSERT INTO tasks VALUES (3,'Najboljša ponudba',4.99);
+INSERT INTO tasks VALUES (4,'Pošljem zbirko',2.99);
 INSERT INTO tasks VALUES (5,'Pošta',2390);
